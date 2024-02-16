a429428a68a126812dab2452e7566821a0b6d8ef
s00196433 <s00196433@mail.itsligo.ie>
2024-02-16 01:57:14 +0000

    FirstCommit

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String says(){
+        return "Hello";
+    }
+   /*  public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+}
---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        App ap = new App();
+       // Assert.assertEquals("Helo", ap.says());
+       Assert.assertEquals("Hello", ap.says());
+       // assertTrue( true );
+    }
+}
